d483c0ca54ea1f5cb259bf5b606862024bc71762
Wang-Zerui <h2010155@nushigh.edu.sg>
2022-09-25 16:20:48 +0800

    Unfinished changes in linking

--- src/main/java/com/zerui/csproject/Controller/MenuController.java
+++ src/main/java/com/zerui/csproject/Controller/MenuController.java
@@ -1,14 +1,20 @@
 package com.zerui.csproject.Controller;
 
+import com.zerui.csproject.SplashScreen;
 import com.zerui.csproject.Utils.Utils;
 import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ProgressIndicator;
 import javafx.scene.control.ScrollBar;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
 import java.io.IOException;
 import java.util.ArrayList;
 
@@ -19,7 +25,6 @@ public class MenuController {
     ScrollPane scrollPane;
     @FXML
     ProgressIndicator loadPosts;
-    ScrollBar scrollBar;
     @FXML
     protected void initialize() throws IOException {
         loadPosts.setVisible(false);
@@ -39,6 +44,17 @@ public class MenuController {
         for (int i = 0; i < 10; i++) postScroll.getChildren().add(Utils.standard.loadPane("/com/zerui/csproject/fxml/userPost.fxml"));
     }
 
+    @FXML
+    protected void createPost() throws IOException {
+        Pane p = FXMLLoader.load(Utils.standard.fxmlPath("createPost.fxml"));
+        Scene scene = new Scene(p, 300, 500);
+        Stage createPost = new Stage();
+        createPost.setScene(scene);
+        createPost.setTitle("Create Post!");
+        createPost.setScene(scene);
+        createPost.show();
+    }
+
     private Pane loadPost(ArrayList<String> imageURLs) throws IOException {
         VBox p = Utils.standard.loadPane("fxml/userPost.fxml");
         p.lookup("");
@@ -49,4 +65,5 @@ public class MenuController {
         }
         return null;
     }
+
 }
