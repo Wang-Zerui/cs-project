07fd611543d36d1135d9c050e4d7547fe6b03f2f
Wang-Zerui <h2010155@nushigh.edu.sg>
2022-10-01 08:58:37 +0800

    Internationalized about programmer

--- src/main/java/com/zerui/csproject/Model/Personal/Friend.java
+++
@@ -1,7 +0,0 @@
-package com.zerui.csproject.Model.Personal;
-
-public class Friend extends AccountModel {
-    Friend(String name, String username, String about, String uuid, String profileLink) {
-        super(name, username, about, uuid, profileLink);
-    }
-}
